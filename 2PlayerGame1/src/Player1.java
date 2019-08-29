29f21d72c6b172617af2a899a8aeed894d7a0921
AlexFAbraham <Alex.f.Abraham@gmail.com>
2019-08-28 20:07:43 -0400

    2 player game

---
+++ 2PlayerGame1/src/Player1.java
@@ -0,0 +1,11 @@
+public class Player1 {
+public static void main(String[] args) {
+
+    System.out.println("      Welcome to Rival Warfare");
+    System.out.println("          Here's the Rules");
+    System.out.println("   Both players start with 100 HEALTH");
+    System.out.println("Both players will take turns Attacking or Healing");
+
+
+}
+}
